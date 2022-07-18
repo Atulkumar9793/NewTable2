2b88497efe91e163b75db76578f1306557439bfa
Atulkumar9793 <atulkumar80551@gmail.com>
2022-07-18 16:50:38 +0530

    Style sheet change line by line

--- App.js
+++ App.js
@@ -53,12 +53,40 @@ export default class ExampleFive extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-  singleHead: { width: 80, height: 40, backgroundColor: '#c8e1ff' },
-  head: { flex: 1, backgroundColor: '#c8e1ff' },
-  title: { flex: 2, backgroundColor: '#f6f8fa' },
-  titleText: { marginRight: 6, textAlign:'right' },
-  text: { textAlign: 'center' },
-  btn: { width: 58, height: 18, marginLeft: 15, backgroundColor: '#c8e1ff', borderRadius: 2 },
-  btnText: { textAlign: 'center' }
+  container: { 
+    flex: 1, 
+    padding: 16,
+    paddingTop: 30,
+    backgroundColor: '#fff',
+    },
+  singleHead: {
+      width: 80,
+      height: 40,
+      backgroundColor: '#c8e1ff',
+      },
+  head: {
+      flex: 1,
+      backgroundColor: '#c8e1ff',
+    },
+  title: { 
+    flex: 2,
+    backgroundColor: '#f6f8fa',
+    },
+  titleText: {
+      marginRight: 6,
+      textAlign:'right',
+      },
+  text: { 
+    textAlign: 'center',
+    },
+  btn: { 
+    width: 58,
+    height: 18,
+    marginLeft: 15,
+    backgroundColor: '#c8e1ff',
+    borderRadius: 2,
+      },
+  btnText: { 
+    textAlign: 'center',
+    },
 });
